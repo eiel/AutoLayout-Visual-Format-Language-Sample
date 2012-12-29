5e85fdde6942ae0c0bd93b20c93df59b869ce61b
Tomohiko Himura <eiel.hal@gmail.com>
2012-12-29 17:08:14 +0900

    タブを作成

---
+++ AutoLayoutSample/ALPhotoFrameViewController.m
@@ -0,0 +1,38 @@
+//
+//  ALPhotoFrameViewController.m
+//  AutoLayoutSample
+//
+//  Created by えいる on 2012/12/29.
+//  Copyright (c) 2012年 Tomohiko Himura. All rights reserved.
+//
+
+#import "ALPhotoFrameViewController.h"
+
+@interface ALPhotoFrameViewController ()
+
+@end
+
+@implementation ALPhotoFrameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
